24a1571147dde6f4a01d0c464af648927a1cf019
dam <kalbarransanchez@danielcastelao.org>
2025-09-22 13:27:55 +0200

    ejercicios

---
+++ exerPython/ejer2.1.py
@@ -0,0 +1,20 @@
+#ejercicio 1 de 2.1
+from CondicionaisBucles import numeros
+
+n1 = 10
+while n1 <= 20 :
+    print(n1)
+    n1 += 1
+
+#ejercicio 2 de 2.1
+amixes= {1: "Dalila", 2: "Mateo", 3:"Angarita", 4:"Saray", 5:"Dani"}
+print()
+print("Amixes: ")
+for clave in amixes:
+    print(amixes[clave])
+
+#ejercicio 3 de 2.1
+
+numeros = [1,2,3,4,5]
+for indice in range(0,5):
+    print(numeros [indice])
---
+++ exerPython/ejer2.4.py
@@ -0,0 +1,3 @@
+"""Utilice o programa anterior para xerar unha táboa de conversión de temperaturas,
+    dende 0º F ata 120º F, de 10 en 10. """
+
